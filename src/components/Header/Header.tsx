d0a16b01c8193bc19521d50187fd11140e8a2600
JoaoPedro <cpsenha@gmail.com>
2025-05-10 23:47:17 -0300

    Basic files for Firebase function have been added. Button with icon at the end component has been modified to show some hover effects and added the option of isLogin button to it, so it will behave and have some different manners. Modal component for the Login (not functional just yet) has been created, the design was made by AI because I'm too bad for design.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import { ButtonWithStartIcon } from "@components/Buttons/ButtonWithStartIcon"
 
 export function Header() {
   return (
-    <div className="p-10 flex justify-between flex-row">
+    <div className="relative p-10 flex justify-between flex-row">
       <h1 className="special-title text-3xl">Sr Gee</h1>
       <div className="flex flex-row gap-12 items-center">
         <a>Pricing</a>
@@ -13,7 +13,7 @@ export function Header() {
           <a className="ml-3">Star us on GitHub!</a>
         </div>
         <button>Sign Up</button>
-        <ButtonWithStartIcon text="Login" icon={ LockKeyhole } />
+        <ButtonWithStartIcon text="Login" icon={ LockKeyhole } isLogin/>
       </div>
     </div>
   )
